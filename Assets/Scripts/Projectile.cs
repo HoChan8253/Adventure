9acd33e381261eac9f22bd55af9b5beee622dcc0
HoChan8253 <nahcohochan@gmail.com>
2026-02-16 02:33:18 +0900

    [Feat] Projectile logic and arrow system
    투사체 로직 구현 및 화살 기능 추가

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 22f;
+
+    private void Update()
+    {
+        MoveProjectile();
+    }
+
+    private void MoveProjectile()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime * _moveSpeed);
+    }
+}
