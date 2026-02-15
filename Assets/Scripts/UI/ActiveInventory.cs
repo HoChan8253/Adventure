290604b0016745d1e8c2e23a843586d65edb5a3c
HoChan8253 <nahcohochan@gmail.com>
2026-02-15 11:08:27 +0900

    [Feat] Ui toggle selection logic
    UI Toggle 선택 상태 제어 추가

---
+++ Assets/Scripts/UI/ActiveInventory.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ActiveInventory : MonoBehaviour
+{
+    private int _activeSlotIndexNum = 0;
+
+    private PlayerControls _playerControls;
+
+    private void Awake()
+    {
+        _playerControls = new PlayerControls();
+    }
+
+    private void Start()
+    {
+        _playerControls.Inventory.Keyboard.performed += ctx => ToggleActiveSlot((int)ctx.ReadValue<float>());
+    }
+
+    private void OnEnable()
+    {
+        _playerControls.Enable();
+    }
+
+    private void ToggleActiveSlot(int numValue)
+    {
+        ToggleActiveHighlight(numValue - 1);
+    }
+
+    private void ToggleActiveHighlight(int indexNum)
+    {
+        _activeSlotIndexNum = indexNum;
+
+        foreach (Transform inventorySlot in this.transform)
+        {
+            inventorySlot.GetChild(0).gameObject.SetActive(false);
+        }
+
+        this.transform.GetChild(indexNum).GetChild(0).gameObject.SetActive(true);
+    }
+}
