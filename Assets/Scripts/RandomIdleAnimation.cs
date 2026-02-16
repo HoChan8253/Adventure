eef8f0b471207c86b45f27f0b5e287c663de0d20
HoChan8253 <nahcohochan@gmail.com>
2026-02-16 15:39:36 +0900

    [Feat] Add ghost enemy
    적 유령 추가

---
+++ Assets/Scripts/RandomIdleAnimation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RandomIdleAnimation : MonoBehaviour
+{
+    private Animator _myAnimator;
+
+    private void Awake()
+    {
+        _myAnimator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        if (!_myAnimator) { return; }
+
+        AnimatorStateInfo state = _myAnimator.GetCurrentAnimatorStateInfo(0);
+        _myAnimator.Play(state.fullPathHash, -1, Random.Range(0f, 1f));
+    }
+}
